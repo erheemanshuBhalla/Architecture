9023d28ff704730fb417c6c19cf94cccd04aa59d
Himanshu Bhalla <erheemanshubhalla@gmail.com>
2025-11-01 17:29:17 +0530

    initi push

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Architecture_V4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
